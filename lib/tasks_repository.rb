82d37ac5ebfcb846fd8c41e10705a612f68893f2
Nathanael Burt <nathanael.burt@gmail.com>
2014-04-03 15:24:23 -0600

    Refactor
    Created a constant to hold database connection
    cleaned up methods using instance variables
    
    Signed-off-by: Martha Berner <marthaberner.mb@gmail.com>

--- lib/tasks_repository.rb
+++ lib/tasks_repository.rb
@@ -1,15 +1,16 @@
 require 'sequel'
 
 class TasksRepository
+  def initialize(db)
+    @db = db
+    @tasks_table = @db[:tasks]
+  end
+
   def insert(task)
-    database = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_manager')
-    tasks_table = database[:tasks]
-    tasks_table.insert(task)
+    @tasks_table.insert(task)
   end
 
   def display_all
-    database = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_manager')
-    tasks_table = database[:tasks]
-    tasks_table.select(:name).to_a
+    @tasks_table.select(:name).to_a
   end
 end
--- spec/tasks_repository_spec.rb
+++ spec/tasks_repository_spec.rb
@@ -1,14 +1,17 @@
 require 'rspec'
 require 'tasks_repository'
 
+
 describe 'it manages tasks' do
-  it 'allows a user to insert information into a database' do
-    tasks = TasksRepository.new
-    database = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_manager')
-    database.create_table! :tasks do
+  before do
+    DB = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_manager')
+    DB.create_table! :tasks do
       primary_key :id
       String :name
     end
+  end
+  it 'allows a user to insert information into a database' do
+    tasks = TasksRepository.new(DB)
     tasks.insert({:name => 'Get milk'})
     tasks.insert({:name => 'Get eggs'})
     expect(tasks.display_all).to eq [{:name => 'Get milk'},{:name => 'Get eggs'}]
